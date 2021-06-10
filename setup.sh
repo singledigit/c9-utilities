c2bcc175199c228ee61838a9789987fa11f3c311
Eric Johnson <ericdj@amazon.com>
2021-06-10 00:56:48 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ mkdir /home/ec2-user/environment/installs
 cd /home/ec2-user/environment/installs
 wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
-sudo ./sam-installation/install --upgrade
+sudo ./sam-installation/install --update
 
 # Install sam-beta-cdk
 mkdir sam-beta-cdk
