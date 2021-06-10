f929d960e40839a757031d9d13f28996934e1ab5
EC2 Default User <ec2-user@ip-172-31-38-38.us-west-2.compute.internal>
2021-06-10 04:18:31 +0000

    updating for setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,35 @@
-sudo yum update
-sudo yum install install util-linux-user zsh
+# Increase hard drive too 100 GB
+./c9sizer.sh 100
+
+# Update installed packages
+sudo yum update -y
+
+# Create Virtual Environments
+mkdir /home/ec2-user/environment/penv
+python3 -m venv /home/ec2-user/environment/penv/sam
+
+# load venv, upgarde pip and install boto3
+source /home/ec2-user/environment/penv/sam/bin/activate
+pip install --upgrade pip
+pip install boto3
+
+# Install latest SAM
+mkdir /home/ec2-user/environment/installs
+cd /home/ec2-user/environment/installs
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install --upgrade
+
+# install required packages
+sudo yum install -y util-linux-user zsh jq
+
+# Change root password
 sudo passwd ec2-user
-chsh -s /bin/zsh
+
+# Install ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Update rc files
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.zshrc ~/.zshrc.bak
+cp ~/c9-utilities/.bashrc ~/c9-utilities/.zshrc ~/
